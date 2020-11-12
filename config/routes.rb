87280b93947d5084609eec8e5fd769b2230d92af
Yasuaki <yasu5293koh@gmail.com>
2020-11-12 08:03:53 +0000

    [Add] post_comments_controller作成、ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :games, only: [:new, :create, :index, :show, :destroy]
+  resources :games, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy] # 投稿に対してコメントされるためpost_commentsは、gamesにネストする
+  end
 end
