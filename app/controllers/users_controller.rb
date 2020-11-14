e8e5d563150e57d7cf3286164ffd3ca3705d4ab8
Yasuaki <yasu5293koh@gmail.com>
2020-11-14 08:28:53 +0000

    [Add] プロフィール編集画面追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+  
   def show
     @user = User.find(params[:id])
     @games = @user.games.page(params[:page]).reverse_order #@userに関連付けられた投稿（.game）のみ@gamesに渡す
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
 end
