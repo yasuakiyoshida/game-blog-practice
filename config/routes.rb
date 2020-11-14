5712cda4a83db2b58acea5de49cfe9f9f44b3767
Yasuaki <yasu5293koh@gmail.com>
2020-11-14 08:17:05 +0000

    [Add] users_controller作成,ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'homes#top'
   resources :games, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy] # いいね機能のルーティング、showページ不要でidの受け渡しも必要ないので単数系resource
     resources :post_comments, only: [:create, :destroy] # 投稿に対してコメントされるためpost_commentsは、gamesにネストする
   end
+  resources :users, only: [:show]
 end
