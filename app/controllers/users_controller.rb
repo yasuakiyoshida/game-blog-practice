a01785afadc447ef7a5d4476e3bb30a91b1e549b
Yasuaki <yasu5293koh@gmail.com>
2020-11-14 08:25:23 +0000

    [Add] マイページ画面追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @games = @user.games.page(params[:page]).reverse_order #@userに関連付けられた投稿（.game）のみ@gamesに渡す
   end
 end
