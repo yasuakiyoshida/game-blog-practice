88adfe1a434ce08265f29201fe24c251328475c2
Yasuaki <yasu5293koh@gmail.com>
2020-11-19 13:13:45 +0000

    [Add] 投稿フォームに星評価追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_14_083028) do
+ActiveRecord::Schema.define(version: 2020_11_19_125702) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_11_14_083028) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "rate"
   end
 
   create_table "post_comments", force: :cascade do |t|
