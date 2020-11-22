c10fdde5cfc1a9fafab1a518bad340e7e3b2c90f
Yasuaki <yasu5293koh@gmail.com>
2020-11-22 07:07:49 +0000

    [Add] 投稿フォームにバリデーションとエラーメッセージ追加

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,8 +7,11 @@ class GamesController < ApplicationController
   def create
     @game = Game.new(game_params)
     @game.user_id = current_user.id
-    @game.save
-    redirect_to games_path
+    if @game.save
+      redirect_to games_path
+    else
+      render :new
+    end
   end
 
   def index
--- app/models/game.rb
+++ app/models/game.rb
@@ -9,6 +9,9 @@ class Game < ApplicationRecord
   attachment :image
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  
+  validates :game_title, presence: true, length: {minimum: 2}
+  validates :impressions, presence: true
 
   def favorited_by?(user) # 引数で渡されたユーザidがFavoritesテーブル内に存在（exists?）するかどうかを調べるメソッドを定義
     favorites.where(user_id: user.id).exists? # モデル名.where(任意のカラム名: 格納されている値) user_idカラムの全レコードを検索し、user.idが含まれているか確認
