a0a290f0623a87158e4fa9b1126ea2a851c298fd
Yasuaki <yasu5293koh@gmail.com>
2020-11-23 08:35:32 +0000

    [Add] usersテーブルにadminカラム追加、初期値はfalse

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_21_075951) do
+ActiveRecord::Schema.define(version: 2020_11_23_082910) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2020_11_21_075951) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
